52409e3125d163a0eff9535eef7d7a6581826ad9
monsterhxw <770342643@qq.com>
2019-11-14 00:57:40 +0800

    feat(/leetcode-0141-linked-list-cycle): using double pointers (Floyd’s Cycle-Finding Algorithm) to detect loop in a linked list.

---
+++ leetcode-0141-linked-list-cycle/src/algorithm/Main.java
@@ -0,0 +1,68 @@
+package algorithm;
+
+/**
+ * @author monstervivi
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        ListNode node = initLinkedList(4);
+        createCycle(node);
+        System.out.println("using double pointers (Floyd’s Cycle-Finding Algorithm) to detect loop in a linked list");
+        System.out.println(hasCycleUsingDoublePointers(node));
+    }
+
+    public static boolean hasCycleUsingDoublePointers(ListNode head) {
+        if (null == head || null == head.next) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = slow.next;
+        while (slow != fast) {
+            if (null == fast || null == fast.next) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+
+    public static void createCycle(ListNode head) {
+        ListNode tail = head;
+        ListNode poiNode = tail.next.next;
+        while (null != tail.next) {
+            tail = tail.next;
+        }
+        tail.next = poiNode;
+    }
+
+    public static ListNode initLinkedList(int num) {
+        int i = 1;
+        ListNode head = new ListNode(i);
+        i++;
+        while (i <= num) {
+            createNodeAtTail(head, i);
+            i++;
+        }
+        return head;
+    }
+
+    public static void createNodeAtTail(ListNode head, int val) {
+        ListNode newNode = new ListNode(val);
+        ListNode tail = head;
+        while (null != tail.next) {
+            tail = tail.next;
+        }
+        tail.next = newNode;
+    }
+
+    private static void printLinkedList(ListNode head) {
+        ListNode cur = head;
+        while (null != cur) {
+            System.out.print(String.format("%d%s", cur.val, " "));
+            cur = cur.next;
+        }
+        System.out.println("\n");
+    }
+}
