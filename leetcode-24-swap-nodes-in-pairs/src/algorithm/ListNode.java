d413ea030901fe0b439d24f08d4b08c83ea1fd7c
monsterhxw <770342643@qq.com>
2019-11-13 17:08:06 +0800

    feat(/leetcode-24-swap-nodes-in-pairs) : add iterative solution, recursive solution, using stack solution.

---
+++ leetcode-24-swap-nodes-in-pairs/src/algorithm/ListNode.java
@@ -0,0 +1,16 @@
+package algorithm;
+
+/**
+ * Definition for singly-linked list.
+ *
+ * @author monstervivi
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
