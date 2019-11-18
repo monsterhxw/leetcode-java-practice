f500a5fa27b08e812cee993ff862ce713c71668a
monsterhxw <770342643@qq.com>
2019-11-18 23:47:42 +0800

    fix(/leetcode-0703-kth-largest-element-in-a-stream): modify incorrect print statements.

--- leetcode-0703-kth-largest-element-in-a-stream/src/algorithm/Main.java
+++ leetcode-0703-kth-largest-element-in-a-stream/src/algorithm/Main.java
@@ -10,9 +10,9 @@ public class Main {
         int[] arr = {4, 5, 8, 2};
         KthLargest kthLargest = new KthLargest(k, arr);
         System.out.println(String.format("kthLargest add 3, return %d", kthLargest.add(3)));
-        System.out.println(String.format("kthLargest add 3, return %d", kthLargest.add(5)));
-        System.out.println(String.format("kthLargest add 3, return %d", kthLargest.add(10)));
-        System.out.println(String.format("kthLargest add 3, return %d", kthLargest.add(9)));
-        System.out.println(String.format("kthLargest add 3, return %d", kthLargest.add(4)));
+        System.out.println(String.format("kthLargest add 5, return %d", kthLargest.add(5)));
+        System.out.println(String.format("kthLargest add 10, return %d", kthLargest.add(10)));
+        System.out.println(String.format("kthLargest add 9, return %d", kthLargest.add(9)));
+        System.out.println(String.format("kthLargest add 4, return %d", kthLargest.add(4)));
     }
 }
