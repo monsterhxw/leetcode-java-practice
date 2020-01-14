1a28985d93bb6e1ea3dd41ce78b56f750dfaabf1
monsterhxw <770342643@qq.com>
2020-01-14 22:31:30 +0800

    feat(/leetcode-0050-powx-n): add using optimizing iteration solution.

--- leetcode-0050-powx-n/src/algorithm/Main.java
+++ leetcode-0050-powx-n/src/algorithm/Main.java
@@ -5,8 +5,25 @@ public class Main {
     public static void main(String[] args) {
         double x = 2;
         int n = 6;
-        System.out.println("using recursion : "+(new Main()).myPowUsingRecursion(x, n));
-        System.out.println("using optimizing recursion : "+(new Main()).myPowUsingRecursionOptimization(x, n));
+        System.out.println("using recursion : " + (new Main()).myPowUsingRecursion(x, n));
+        System.out.println("using optimizing recursion : " + (new Main()).myPowUsingRecursionOptimization(x, n));
+        System.out.println("using iteration : " + (new Main()).myPowUsingIteration(x, n));
+    }
+
+    public double myPowUsingIteration(double x, int n) {
+        if (n == 0) {
+            return 1.0d;
+        }
+        long N = Math.abs((long)n);
+        double answer = 1.0d;
+        while (N > 0) {
+            if ((N & 1) == 1) {
+                answer = answer * x;
+            }
+            N = N >> 1;
+            x = x * x;
+        }
+        return N < 0 ? 1 / answer : answer;
     }
 
     public double myPowUsingRecursionOptimization(double x, int n) {
