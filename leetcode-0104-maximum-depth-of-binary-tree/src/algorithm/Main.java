02710998097d468661492859d43066d4eeec3d7f
monsterhxw <770342643@qq.com>
2020-01-19 16:23:04 +0800

    feat(/lleetcode-0104-maximum-depth-of-binary-tree): add using bfs and iteration solutions.

--- leetcode-0104-maximum-depth-of-binary-tree/src/algorithm/Main.java
+++ leetcode-0104-maximum-depth-of-binary-tree/src/algorithm/Main.java
@@ -1,5 +1,9 @@
 package algorithm;
 
+import com.sun.tools.hat.internal.model.Root;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author monstervivi
  */
@@ -9,10 +13,34 @@ public class Main {
         Main main = new Main();
         TreeNode root = main.createBinaryTree();
         System.out.println("using DFS : " + main.maxDepthUsingDFS(root));
+        System.out.println("using BFS : " + main.maxDepthUsingBFS(root));
     }
 
     public int maxDepthUsingDFS(TreeNode root) {
         return root == null ? 0 : 1 + Math.max(maxDepthUsingDFS(root.left), maxDepthUsingDFS(root.right));
+    }
+
+    public int maxDepthUsingBFS(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int depth = 0;
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
     }
 
     private TreeNode createBinaryTree() {
