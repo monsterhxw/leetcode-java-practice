86c27503a28b0738f2deda083568c0f73f692ebf
monsterhxw <770342643@qq.com>
2020-01-21 11:22:16 +0800

    feat(/leetcode-0022-generate-parentheses): add thinking comments.

--- leetcode-0022-generate-parentheses/src/algorithm/Main.java
+++ leetcode-0022-generate-parentheses/src/algorithm/Main.java
@@ -9,9 +9,26 @@ public class Main {
   public static void main(String[] args) {
     Main main = new Main();
     int n = 3;
-    System.out.println(main.generateParenthesis(n));
+    System.out.println("using dfs recursion: " + main.generateParenthesisUsingDFS(n));
   }
 
+  public List<String> generateParenthesisUsingDFS(int n) {
+    if (n == 0) {
+      return new ArrayList<>();
+    }
+    List<String> result = new ArrayList<>();
+    dfsHelper(n, n, "", result);
+    return result;
+  }
+
+  /**
+   * 回溯算法 DFS
+   *
+   * @param left 左括号可用数量
+   * @param right 右括号可用数量
+   * @param subStr 当前递归的结果
+   * @param result 结果集
+   */
   //                      (
   //             /                  \
   //          ((                     ()
@@ -21,23 +38,18 @@ public class Main {
   //  ((()))  (()(   (())     ()((      ()()
   //            |      |        |         |
   //         (()())  (())()   ()(())    ()()()
-  public List<String> generateParenthesis(int n) {
-    if (n == 0) {
-      return new ArrayList<>();
-    }
-    List<String> result = new ArrayList<>();
-    dfsHelper(n, n, "", result);
-    return result;
-  }
-
   private void dfsHelper(int left, int right, String subStr, List<String> result) {
+    // 如果左括号可用数量为 0 而且右括号可用数量也为 0，那么就是找出有效的括号
     if (left == 0 && right == 0) {
       result.add(subStr);
       return;
     }
+    // 若做左括号可用数量大于 0，填入左括号
     if (left > 0) {
       dfsHelper(left - 1, right, subStr + "(", result);
     }
+    // 只有当右括号可用数量大于左括号可用数量的时候，才能填入右括号
+    // 如 ()_ 就不能填入 )，否则 ()) 就是一个错误的括号
     if (right > left) {
       dfsHelper(left, right - 1, subStr + ")", result);
     }
