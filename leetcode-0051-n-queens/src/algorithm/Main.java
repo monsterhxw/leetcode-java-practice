08adb66578b97c3a7aad10f9cd8480a11b2d9df0
monsterhxw <770342643@qq.com>
2020-01-30 20:57:44 +0800

    feat(/leetcode-0051-n-queens): add using dfs and bit solution.

--- leetcode-0051-n-queens/src/algorithm/Main.java
+++ leetcode-0051-n-queens/src/algorithm/Main.java
@@ -9,16 +9,21 @@ import java.util.List;
  */
 public class Main {
 
-  private List<List<String>> result = new ArrayList<>();
+  private List<List<String>> resultUsingArrays = new ArrayList<>();
+  private List<List<String>> resultsUsingBit = new ArrayList<>();
+  private List<String> resultUsingBit = new ArrayList<>();
 
   public static void main(String[] args) {
     Main main = new Main();
     int n = 4;
     System.out.println("using dfs and arrays : ");
-    main.solveNQueensUsingDFSANDArrays(n).forEach(System.out::println);
+    main.solveNQueensUsingDFSAndArrays(n).forEach(System.out::println);
+    System.out.println("\n");
+    System.out.println("using dfs and bit : ");
+    main.solveNQueensUsingDFSAndBit(n).forEach(System.out::println);
   }
 
-  public List<List<String>> solveNQueensUsingDFSANDArrays(int n) {
+  public List<List<String>> solveNQueensUsingDFSAndArrays(int n) {
     if (n <= 0) {
       return new ArrayList<>();
     }
@@ -31,12 +36,13 @@ public class Main {
 
     backTrackUsingDFSAndArrays(0, n, col, sum, diff, board);
 
-    return result;
+    return resultUsingArrays;
   }
 
-  private void backTrackUsingDFSAndArrays(int depth, int n, int[] col, int[] sum, int[] diff, List<String> board) {
+  private void backTrackUsingDFSAndArrays(int depth, int n, int[] col, int[] sum, int[] diff,
+      List<String> board) {
     if (depth == n) {
-      result.add(new ArrayList<>(board));
+      resultUsingArrays.add(new ArrayList<>(board));
       return;
     }
 
@@ -60,5 +66,65 @@ public class Main {
         diff[depth - column + n - 1] = 0;
       }
     }
+  }
+
+  public List<List<String>> solveNQueensUsingDFSAndBit(int n) {
+    if (n <= 0) {
+      return new ArrayList<>();
+    }
+
+    backTrackUsingDFSAndBit(n, 0, 0, 0);
+
+    return resultsUsingBit;
+  }
+
+  private void backTrackUsingDFSAndBit(int n, int col, int pie, int na) {
+    if (n == resultUsingBit.size()) {
+      resultsUsingBit.add(new ArrayList<>(resultUsingBit));
+      return;
+    }
+
+    // 获得所有的有效空位
+    // (col | pie | na) 可以得到所有被占的空位，取反之后将有效空位置为 1
+    // 与上 (1 << n) - 1，是设定考虑范围，比如 8 皇后，那么只用考虑低 8 位即可
+    int bit = ((~(col | pie | na)) & ((1 << n) - 1));
+
+    // bit > 0 表示有空位
+    while (bit > 0) {
+      // 选择最低位的一个空位
+      int tmp = bit & (-bit);
+
+      // 构建当前行的答案
+      String str = constructString(tmp, n);
+      resultUsingBit.add(str);
+
+      // col | tmp 是将 col 中当前选择的这一列置为 1，也就是无效
+      // (pie | tmp) << 1 是设置之前行和当前行对左下的影响
+      // (na | tmp) >> 1 是设置之前行和当前行对右下的影响
+      backTrackUsingDFSAndBit(n, col | tmp, (pie | tmp) << 1, (na | tmp) >> 1);
+
+      resultUsingBit.remove(resultUsingBit.size() - 1);
+
+      // 将当前选择的这个最低位置为 0
+      bit &= bit - 1;
+    }
+  }
+
+  private String constructString(int i, int n) {
+    char[] row = new char[n];
+
+    Arrays.fill(row, '.');
+    int tmp = 1, index = 0;
+
+    while (i != 0) {
+      if ((tmp & i) != 0) {
+        row[index] = 'Q';
+      }
+
+      i >>= 1;
+      index++;
+    }
+
+    return new String(row);
   }
 }
