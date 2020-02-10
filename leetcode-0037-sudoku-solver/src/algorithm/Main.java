509d2932f16eb81188d1b3fa464481237606849a
monsterhxw <770342643@qq.com>
2020-02-11 03:43:12 +0800

    fix(/leetcode-0037-sudoku-solver): add optimization solution.

--- leetcode-0037-sudoku-solver/src/algorithm/Main.java
+++ leetcode-0037-sudoku-solver/src/algorithm/Main.java
@@ -1,10 +1,11 @@
 package algorithm;
 
-import com.sun.org.apache.bcel.internal.generic.NEW;
-import com.sun.org.apache.xpath.internal.functions.FuncFalse;
-
 /** @author monstervivi */
 public class Main {
+
+  boolean[][] rows = new boolean[9][9];
+  boolean[][] cols = new boolean[9][9];
+  boolean[][] boxes = new boolean[9][9];
 
   public static void main(String[] args) {
     char[][] board =
@@ -32,43 +33,56 @@ public class Main {
     if (board.length == 0 || board == null) {
       return;
     }
-    solver(board);
-  }
 
-  private boolean solver(char[][] board) {
+    // 初始化
     for (int i = 0; i < board.length; i++) {
       for (int j = 0; j < board[i].length; j++) {
         if (board[i][j] == '.') {
-          for (char c = '1'; c <= '9'; c++) {
-            if (isValid(board, i, j, c)) {
-              board[i][j] = c;
-              if (solver(board)) {
-                return true;
-              } else {
-                board[i][j] = '.';
-              }
-            }
-          }
-          return false;
+          continue;
         }
+        // num 为二维数组 row, col, block 的列下标，所以这里处理为 - 49
+        int num = board[i][j] - 49;
+        int boxesIndex = (i / 3) * 3 + (j / 3);
+        rows[i][num] = true;
+        cols[j][num] = true;
+        boxes[boxesIndex][num] = true;
       }
     }
-    return true;
+    solver(board, 0, 0);
   }
 
-  private boolean isValid(char[][] board, int row, int col, char placeNumber) {
-    for (int i = 0; i < board.length; i++) {
-      if (board[row][i] != '.' && board[row][i] == placeNumber) {
-        return false;
-      }
-      if (board[i][col] != '.' && board[i][col] == placeNumber) {
-        return false;
-      }
-      if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] != '.'
-          && board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == placeNumber) {
-        return false;
+  private boolean solver(char[][] board, int row, int col) {
+    if (col == board[row].length) {
+      col = 0;
+      row++;
+      if (row == board.length) {
+        return true;
       }
     }
-    return true;
+
+    // 是空则尝试填充, 否则跳过继续尝试填充下一个位置
+    if (board[row][col] == '.') {
+      // 尝试填充 1 ~ 9
+      int boxesIndex = (row / 3) * 3 + col / 3;
+      for (char c = '1'; c <= '9'; c++) {
+        if (!rows[row][c - 49] && !cols[col][c - 49] && !boxes[boxesIndex][c - 49]) {
+          board[row][col] = c;
+          rows[row][c - 49] = true;
+          cols[col][c - 49] = true;
+          boxes[boxesIndex][c - 49] = true;
+          if (solver(board, row, col + 1)) {
+            return true;
+          }
+          // 剪枝
+          board[row][col] = '.';
+          rows[row][c - 49] = false;
+          cols[col][c - 49] = false;
+          boxes[boxesIndex][c - 49] = false;
+        }
+      }
+    } else {
+      return solver(board, row, col + 1);
+    }
+    return false;
   }
 }
