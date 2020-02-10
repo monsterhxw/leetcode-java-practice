978c6afcc7e20a53984dae95abf83b02f691981f
monsterhxw <770342643@qq.com>
2020-02-11 00:10:22 +0800

    fix(/leetcode-0037-sudoku-solver): add some print statements.

--- leetcode-0037-sudoku-solver/src/algorithm/Main.java
+++ leetcode-0037-sudoku-solver/src/algorithm/Main.java
@@ -20,7 +20,12 @@ public class Main {
           {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
         };
     (new Main()).solveSudoku(board);
-    System.out.println();
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[i].length; j++) {
+        System.out.print(board[i][j] + " ");
+      }
+      System.out.println();
+    }
   }
 
   public void solveSudoku(char[][] board) {
@@ -28,7 +33,6 @@ public class Main {
       return;
     }
     solver(board);
-    System.out.println();
   }
 
   private boolean solver(char[][] board) {
