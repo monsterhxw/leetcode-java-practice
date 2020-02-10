66df038e4d10f310692b25ddc925b22b3832df71
monsterhxw <770342643@qq.com>
2020-02-10 23:59:18 +0800

    feat(/leetcode-0037-sudoku-solver): add using dfs solution.

---
+++ leetcode-0037-sudoku-solver/src/algorithm/Main.java
@@ -0,0 +1,70 @@
+package algorithm;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+import com.sun.org.apache.xpath.internal.functions.FuncFalse;
+
+/** @author monstervivi */
+public class Main {
+
+  public static void main(String[] args) {
+    char[][] board =
+        new char[][] {
+          {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+          {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+          {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+          {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+          {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+          {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+          {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+          {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+          {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+    (new Main()).solveSudoku(board);
+    System.out.println();
+  }
+
+  public void solveSudoku(char[][] board) {
+    if (board.length == 0 || board == null) {
+      return;
+    }
+    solver(board);
+    System.out.println();
+  }
+
+  private boolean solver(char[][] board) {
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[i].length; j++) {
+        if (board[i][j] == '.') {
+          for (char c = '1'; c <= '9'; c++) {
+            if (isValid(board, i, j, c)) {
+              board[i][j] = c;
+              if (solver(board)) {
+                return true;
+              } else {
+                board[i][j] = '.';
+              }
+            }
+          }
+        }
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private boolean isValid(char[][] board, int row, int col, char placeNumber) {
+    for (int i = 0; i < board.length; i++) {
+      if (board[row][i] != '.' && board[row][i] == placeNumber) {
+        return false;
+      }
+      if (board[i][col] != '.' && board[i][col] == placeNumber) {
+        return false;
+      }
+      if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] != '.'
+          && board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == placeNumber) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
