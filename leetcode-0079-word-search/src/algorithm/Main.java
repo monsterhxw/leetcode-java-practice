6383bad97ed87f329f3e2bad442b6a85ad0e0d08
monsterhxw <770342643@qq.com>
2020-02-15 01:50:50 +0800

    feat(/leetcode-0079-word-search): Optimize the code.

--- leetcode-0079-word-search/src/algorithm/Main.java
+++ leetcode-0079-word-search/src/algorithm/Main.java
@@ -1,14 +1,33 @@
 package algorithm;
 
+import java.util.Arrays;
+import java.util.List;
+
 /** @author monstervivi */
 public class Main {
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    char[][] board =
+        new char[][] {
+          {'A', 'B', 'C', 'E'},
+          {'S', 'F', 'C', 'S'},
+          {'A', 'D', 'E', 'E'}
+        };
+
+    Main main = new Main();
+
+    List<String> words = Arrays.asList("ABCCED", "SEE", "ABCB");
+
+    for (String word : words) {
+      System.out.println("word is : " + word + ", result : " + main.exist(board, word));
+    }
+  }
 
   public boolean exist(char[][] board, String word) {
     if (board == null
-        || board.length < 1
-        || word.length() < 1
+        || board.length == 0
+        || word == null
+        || word.length() == 0
         || board.length * board[0].length < word.length()) {
       return false;
     }
@@ -16,7 +35,7 @@ public class Main {
     for (int i = 0; i < board.length; i++) {
       for (int j = 0; j < board[i].length; j++) {
         if (board[i][j] == word.charAt(0)) {
-          if (dfsBacktrace(board, i, j, word, 0)) {
+          if (dfsBacktracking(board, i, j, word, 0)) {
             return true;
           }
         }
@@ -26,7 +45,7 @@ public class Main {
     return false;
   }
 
-  private boolean dfsBacktrace(char[][] board, int row, int col, String word, int curIndex) {
+  private boolean dfsBacktracking(char[][] board, int row, int col, String word, int curIndex) {
     if (curIndex == word.length()) {
       return true;
     }
@@ -34,7 +53,7 @@ public class Main {
     if (row < 0
         || col < 0
         || row >= board.length
-        || col >= board[0].length
+        || col >= board[row].length
         || board[row][col] != word.charAt(curIndex)) {
       return false;
     }
@@ -42,10 +61,10 @@ public class Main {
     board[row][col] = '.';
 
     boolean res =
-        dfsBacktrace(board, row - 1, col, word, curIndex + 1)
-            || dfsBacktrace(board, row + 1, col, word, curIndex + 1)
-            || dfsBacktrace(board, row, col - 1, word, curIndex + 1)
-            || dfsBacktrace(board, row, col + 1, word, curIndex + 1);
+        dfsBacktracking(board, row - 1, col, word, curIndex + 1)
+            || dfsBacktracking(board, row + 1, col, word, curIndex + 1)
+            || dfsBacktracking(board, row, col - 1, word, curIndex + 1)
+            || dfsBacktracking(board, row, col + 1, word, curIndex + 1);
 
     board[row][col] = word.charAt(curIndex);
 
