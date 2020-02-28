010d55487a72e021534ccf1f458a0a5f02db015f
monsterhxw <770342643@qq.com>
2020-02-28 23:32:00 +0800

    feat(/leetcode-0070-climbing-stairs): add using dynamic programming solution.

---
+++ leetcode-0070-climbing-stairs/src/algorithm/Main.java
@@ -0,0 +1,21 @@
+package algorithm;
+
+/** @author monstervivi */
+public class Main {
+
+  public static void main(String[] args) {
+    Main main = new Main();
+    int n = 3;
+    System.out.println("using dynamic programming : result is " + main.climbStairs(n));
+  }
+
+  public int climbStairs(int n) {
+    int[] dp = new int[n + 1];
+    dp[0] = 1;
+    dp[1] = 1;
+    for (int i = 2; i <= n; ++i) {
+      dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+  }
+}
