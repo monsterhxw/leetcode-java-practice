629dd4ed98941612338eaf24619d314190b2e34c
monsterhxw <770342643@qq.com>
2020-03-01 23:46:31 +0800

    feat(/leetcode-0070-climbing-stairs): add using dynamic programming optimization solution.

--- leetcode-0070-climbing-stairs/src/algorithm/Main.java
+++ leetcode-0070-climbing-stairs/src/algorithm/Main.java
@@ -24,7 +24,7 @@ public class Main {
     return climb_Stairs(0, n, memo);
   }
 
-  public int climb_Stairs(int i, int n, int memo[]) {
+  public int climbStairs(int i, int n, int memo[]) {
     if (i > n) {
       return 0;
     }
@@ -34,7 +34,24 @@ public class Main {
     if (memo[i] > 0) {
       return memo[i];
     }
-    memo[i] = climb_Stairs(i + 1, n, memo) + climb_Stairs(i + 2, n, memo);
+    memo[i] = climbStairs(i + 1, n, memo) + climbStairs(i + 2, n, memo);
     return memo[i];
+  }
+
+  public int climbStairsUsingDPOptimization(int n) {
+    if (n == 1) {
+      return 1;
+    }
+
+    int first = 1;
+    int second = 2;
+
+    for (int i = 3; i <= n; i++) {
+      int third = first + second;
+      first = second;
+      second = third;
+    }
+
+    return second;
   }
 }
