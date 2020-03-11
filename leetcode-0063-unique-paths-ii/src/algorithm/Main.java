11cbf9d73f22a4f76d7b9d23a74cba4a59871131
monsterhxw <770342643@qq.com>
2020-03-11 14:33:28 +0800

    feat(/leetcode-0063-unique-paths-ii): add using memoization search solution.

--- leetcode-0063-unique-paths-ii/src/algorithm/Main.java
+++ leetcode-0063-unique-paths-ii/src/algorithm/Main.java
@@ -1,5 +1,7 @@
 package algorithm;
 
+import java.util.Arrays;
+
 /** @author monstervivi */
 public class Main {
 
@@ -19,6 +21,43 @@ public class Main {
     System.out.println(
         "using dfs and backtracking, result is "
             + main.uniquePathsWithObstaclesUsingDFSAndBacktracking(obstacleGrid));
+
+    System.out.println(
+        "using memoization search, result is "
+            + main.uniquePathsWithObstacleUsingMemoizationSearch(obstacleGrid));
+  }
+
+  public int uniquePathsWithObstacleUsingMemoizationSearch(int[][] obstacleGrid) {
+    if (obstacleGrid == null
+        || obstacleGrid[obstacleGrid.length - 1][obstacleGrid[0].length - 1] == 1) {
+      return 0;
+    }
+
+    if (obstacleGrid.length == 0 && obstacleGrid[0].length == 0) {
+      return 1;
+    }
+
+    Integer[][] memo = new Integer[obstacleGrid.length][obstacleGrid[0].length];
+
+    return dfsHelper(0, 0, obstacleGrid, memo);
+  }
+
+  private int dfsHelper(int row, int col, int[][] grid, Integer[][] memo) {
+    if (row >= grid.length || col >= grid[0].length || grid[row][col] == 1) {
+      return 0;
+    }
+
+    if (row == grid.length - 1 && col == grid[0].length - 1) {
+      return 1;
+    }
+
+    if (memo[row][col] != null) {
+      return memo[row][col];
+    }
+
+    memo[row][col] = dfsHelper(row + 1, col, grid, memo) + dfsHelper(row, col + 1, grid, memo);
+
+    return memo[row][col];
   }
 
   public int uniquePathsWithObstaclesUsingDFSAndBacktracking(int[][] obstacleGrid) {
