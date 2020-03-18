fb9ebc26f93533bd28659431e3d48f770e1185d2
monsterhxw <770342643@qq.com>
2020-03-18 23:28:17 +0800

    feat(/leetcode-0279-perfect-squares): add using dynamic programming solution.

--- leetcode-0279-perfect-squares/src/algorithm/Main.java
+++ leetcode-0279-perfect-squares/src/algorithm/Main.java
@@ -15,6 +15,22 @@ public class Main {
     System.out.println("n is : " + n);
 
     System.out.println("using bfs, result is : " + main.numSquaresUsingBFS(n));
+
+    System.out.println("using dynamic programming, result is : " + main.numSquaresUsingDP(n));
+  }
+
+  public int numSquaresUsingDP(int n) {
+    int[] dp = new int[n + 1];
+    dp[0] = 0;
+
+    for (int i = 1; i <= n; i++) {
+      dp[i] = i;
+      for (int j = 1; i - j * j >= 0; j++) {
+        dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+      }
+    }
+
+    return dp[n];
   }
 
   public int numSquaresUsingBFS(int n) {
