3eb293396229155f313796ba997f7dc3ddd273c1
monsterhxw <770342643@qq.com>
2020-03-26 05:08:20 +0800

    feat(/leetcode-0204-count-primes): add using "Sieve of Eratosthenes" solution.

---
+++ leetcode-0204-count-primes/src/algorithm/Main.java
@@ -0,0 +1,65 @@
+package algorithm;
+
+import java.util.Arrays;
+
+/** @author monstervivi */
+public class Main {
+
+  public static void main(String[] args) {
+    Main main = new Main();
+
+    int n = 499979;
+
+    System.out.println("n is : " + n);
+
+    System.out.println("inefficient approach, result is : " + main.countPrimesInefficient(n));
+
+    System.out.println("efficient approach, result is : " + main.countPrimesEfficient(n));
+  }
+
+  public int countPrimesEfficient(int n) {
+    if (n <= 2) {
+      return 0;
+    }
+
+    int count = 0;
+
+    boolean[] notPrime = new boolean[n];
+
+    for (int i = 2; i < n; i++) {
+      if (notPrime[i]) {
+        continue;
+      }
+      count++;
+      for (int j = 2; j * i < n; j ++) {
+        notPrime[i * j] = true;
+      }
+    }
+
+    return count;
+  }
+
+  public int countPrimesInefficient(int n) {
+    if (n <= 1) {
+      return 0;
+    }
+
+    int count = 0;
+    for (int i = 2; i < n; i++) {
+      if (isPrime(i)) {
+        count++;
+      }
+    }
+
+    return count;
+  }
+
+  private boolean isPrime(int n) {
+    for (int j = 2; j <= Math.sqrt(n); j++) {
+      if (n % j == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
