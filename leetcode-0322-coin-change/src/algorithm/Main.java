e530b89b538d8bcb4e2d83f3a855519980574e52
monsterhxw <770342643@qq.com>
2020-03-28 23:37:12 +0800

    feat(/leetcode-0322-coin-change): add using memoization search solution.

--- leetcode-0322-coin-change/src/algorithm/Main.java
+++ leetcode-0322-coin-change/src/algorithm/Main.java
@@ -4,7 +4,7 @@ package algorithm;
 public class Main {
 
   public static void main(String[] args) {
-    int[] coins = new int[] {1, 2};
+    int[] coins = new int[] {1, 2, 5};
     int amount = 11;
     System.out.print("coins is ");
     for (int coin : coins) {
@@ -14,8 +14,12 @@ public class Main {
     System.out.println("amount is " + amount);
 
     Main main = new Main();
+
     System.out.println(
         "using recursive, result is " + main.coinChangeUsingRecursive(coins, amount));
+
+    System.out.println(
+        "using memoization search, result is " + main.coinChangeUsingMemoization(coins, amount));
   }
 
   public int coinChangeUsingRecursive(int[] coins, int amount) {
@@ -30,9 +34,51 @@ public class Main {
         continue;
       }
       int ret = coinChangeUsingRecursive(coins, amount - coin);
-      result = Math.min(result, ret == -1 ? -1 : ret + 1);
+      if (ret == -1) {
+        continue;
+      }
+      result = Math.min(result, ret + 1);
     }
 
     return result == Integer.MAX_VALUE ? -1 : result;
+  }
+
+  public int coinChangeUsingMemoization(int[] coins, int amount) {
+    if (coins == null || coins.length == 0) {
+      return -1;
+    }
+
+    Integer[] memo = new Integer[amount + 1];
+
+    return dfsHelper(coins, amount, memo);
+  }
+
+  private int dfsHelper(int[] coins, int amount, Integer[] memo) {
+    if (amount == 0) {
+      return 0;
+    }
+
+    if (memo[amount] != null) {
+      return memo[amount];
+    }
+
+    int result = Integer.MAX_VALUE;
+
+    for (int coin : coins) {
+      if (amount < coin) {
+        continue;
+      }
+      int ret = dfsHelper(coins, amount - coin, memo);
+      if (ret == -1) {
+        continue;
+      }
+      result = Math.min(result, ret + 1);
+    }
+
+    result = result == Integer.MAX_VALUE ? -1 : result;
+
+    memo[amount] = result;
+
+    return memo[amount];
   }
 }
