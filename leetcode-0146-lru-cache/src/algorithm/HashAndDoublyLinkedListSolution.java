6493ffd948af644e1f36bdd6a15b152d1b7a64ae
huangxuewei <huangxueweihxw@gmail.com>
2021-07-02 02:22:49 +0800

    format code.

--- leetcode-0146-lru-cache/src/algorithm/HashAndDoublyLinkedListSolution.java
+++ leetcode-0146-lru-cache/src/algorithm/HashAndDoublyLinkedListSolution.java
@@ -10,107 +10,107 @@ import java.util.Map;
  */
 public class HashAndDoublyLinkedListSolution {
 
-    private final int capacity;
+  private final int capacity;
 
-    static class Entry {
+  static class Entry {
 
-        private int key;
-        private int value;
-        private Entry previous;
-        private Entry next;
+    private int key;
+    private int value;
+    private Entry previous;
+    private Entry next;
 
-        Entry(int key, int value) {
-            this.key = key;
-            this.value = value;
-        }
+    Entry(int key, int value) {
+      this.key = key;
+      this.value = value;
     }
+  }
 
-    private Map<Integer, Entry> map;
-    private final Entry lruEnd;
-    private final Entry mruEnd;
+  private Map<Integer, Entry> map;
+  private final Entry lruEnd;
+  private final Entry mruEnd;
 
-    public HashAndDoublyLinkedListSolution(int capacity) {
-        // initialize capacity
-        this.capacity = capacity;
-        // initialize hash map
-        map = new HashMap<>((int) (capacity * 0.75 + 1), 0.75f);
-        // initialize LRU_END and MRU_END
-        lruEnd = new Entry(0, 0);
-        mruEnd = new Entry(0, 0);
-        // initialize doubly linked list
-        lruEnd.next = mruEnd;
-        mruEnd.previous = lruEnd;
+  public HashAndDoublyLinkedListSolution(int capacity) {
+    // initialize capacity
+    this.capacity = capacity;
+    // initialize hash map
+    map = new HashMap<>((int) (capacity * 0.75 + 1), 0.75f);
+    // initialize LRU_END and MRU_END
+    lruEnd = new Entry(0, 0);
+    mruEnd = new Entry(0, 0);
+    // initialize doubly linked list
+    lruEnd.next = mruEnd;
+    mruEnd.previous = lruEnd;
+  }
+
+  public int get(int key) {
+    if (map.containsKey(key)) {
+      Entry entry = map.get(key);
+      popToMRUEnd(entry);
+      return entry.value;
+    } else {
+      return -1;
     }
+  }
 
-    public int get(int key) {
-        if (map.containsKey(key)) {
-            Entry entry = map.get(key);
-            popToMRUEnd(entry);
-            return entry.value;
-        } else {
-            return -1;
-        }
+  public void put(int key, int value) {
+    if (map.containsKey(key)) {
+      Entry entry = map.get(key);
+      entry.value = value;
+      popToMRUEnd(entry);
+    } else {
+      Entry newEntry = new Entry(key, value);
+      if (map.size() >= capacity) {
+        Entry evicted = evictLRUEnd();
+        map.remove(evicted.key);
+      }
+      putToMRUEnd(newEntry);
+      map.put(key, newEntry);
     }
+  }
 
-    public void put(int key, int value) {
-        if (map.containsKey(key)) {
-            Entry entry = map.get(key);
-            entry.value = value;
-            popToMRUEnd(entry);
-        } else {
-            Entry newEntry = new Entry(key, value);
-            if (map.size() >= capacity) {
-                Entry evicted = evictLRUEnd();
-                map.remove(evicted.key);
-            }
-            putToMRUEnd(newEntry);
-            map.put(key, newEntry);
-        }
+  private void popToMRUEnd(Entry entry) {
+    if (entry == mruEnd.previous) {
+      // do nothing
+    } else {
+      removeEntry(entry);
+      putToMRUEnd(entry);
     }
+  }
 
-    private void popToMRUEnd(Entry entry) {
-        if (entry == mruEnd.previous) {
-            // do nothing
-        } else {
-            removeEntry(entry);
-            putToMRUEnd(entry);
-        }
-    }
+  private void removeEntry(Entry entry) {
+    entry.previous.next = entry.next;
+    entry.next.previous = entry.previous;
+  }
 
-    private void removeEntry(Entry entry) {
-        entry.previous.next = entry.next;
-        entry.next.previous = entry.previous;
-    }
+  private void putToMRUEnd(Entry entry) {
+    entry.previous = mruEnd.previous;
+    entry.next = mruEnd;
+    mruEnd.previous.next = entry;
+    mruEnd.previous = entry;
+  }
 
-    private void putToMRUEnd(Entry entry) {
-        entry.previous = mruEnd.previous;
-        entry.next = mruEnd;
-        mruEnd.previous.next = entry;
-        mruEnd.previous = entry;
+  private Entry evictLRUEnd() {
+    if (lruEnd.next == mruEnd) {
+      return null;
+    } else {
+      Entry entry = lruEnd.next;
+      removeEntry(lruEnd.next);
+      return entry;
     }
+  }
 
-    private Entry evictLRUEnd() {
-        if (lruEnd.next == mruEnd) {
-            return null;
-        } else {
-            Entry entry = lruEnd.next;
-            removeEntry(lruEnd.next);
-            return entry;
-        }
+  public void printDLList() {
+    Entry current = this.lruEnd.next;
+    if (null == current) {
+      System.out.println("Doubly linked list is empty!");
+    } else {
+      System.out.print("Entries: [LRU End - ");
+      while (null != current && null != current.next) {
+        System.out.print("Entry(key:" + current.key + ", value:" + current.value + ") ");
+        current = current.next;
+      }
+      System.out.print("- MRU End]");
+      System.out.println();
     }
-
-    public void printDLList() {
-        Entry current = this.lruEnd.next;
-        if (null == current) {
-            System.out.println("Doubly linked list is empty!");
-        } else {
-            System.out.print("Entries: [LRU End - ");
-            while (null != current && null != current.next) {
-                System.out.print("Entry(key:" + current.key + ", value:" + current.value + ") ");
-                current = current.next;
-            }
-            System.out.print("- MRU End]");
-            System.out.println();
-        }
-    }
+  }
 }
--- leetcode-0146-lru-cache/src/algorithm/Test.java
+++ leetcode-0146-lru-cache/src/algorithm/Test.java
@@ -6,44 +6,44 @@ package algorithm;
  */
 public class Test {
 
-    public static void main(String[] args) {
-        testHashAndDoublyLinkedListSolution();
-    }
+  public static void main(String[] args) {
+    testHashAndDoublyLinkedListSolution();
+  }
 
-    private static void testHashAndDoublyLinkedListSolution() {
-        System.out.println("--------------testHashAndDoublyLinkedListSolution--------------");
-        HashAndDoublyLinkedListSolution cache = new HashAndDoublyLinkedListSolution(2);
-        System.out.println("Invoke put(1, 1) and put(2, 2)");
-        cache.put(1, 1);
-        cache.put(2, 2);
-        cache.printDLList();
+  private static void testHashAndDoublyLinkedListSolution() {
+    System.out.println("--------------testHashAndDoublyLinkedListSolution--------------");
+    HashAndDoublyLinkedListSolution cache = new HashAndDoublyLinkedListSolution(2);
+    System.out.println("Invoke put(1, 1) and put(2, 2)");
+    cache.put(1, 1);
+    cache.put(2, 2);
+    cache.printDLList();
 
-        System.out.println();
-        System.out.println("Invoke get(1), value is: " + cache.get(1));
-        cache.printDLList();
+    System.out.println();
+    System.out.println("Invoke get(1), value is: " + cache.get(1));
+    cache.printDLList();
 
-        System.out.println();
-        System.out.println("Invoke put(3, 3)");
-        cache.put(3, 3);
-        cache.printDLList();
+    System.out.println();
+    System.out.println("Invoke put(3, 3)");
+    cache.put(3, 3);
+    cache.printDLList();
 
-        System.out.println();
-        System.out.println("Invoke get(2), value is: " + cache.get(2));
-        cache.printDLList();
+    System.out.println();
+    System.out.println("Invoke get(2), value is: " + cache.get(2));
+    cache.printDLList();
 
-        System.out.println();
-        System.out.println("Invoke get(3), value is: " + cache.get(3));
-        cache.printDLList();
+    System.out.println();
+    System.out.println("Invoke get(3), value is: " + cache.get(3));
+    cache.printDLList();
 
-        System.out.println();
-        System.out.println("Invoke put(4, 4)");
-        cache.put(4, 4);
-        cache.printDLList();
+    System.out.println();
+    System.out.println("Invoke put(4, 4)");
+    cache.put(4, 4);
+    cache.printDLList();
 
-        System.out.println();
-        System.out.println("Invoke get(1), value is: " + cache.get(1));
-        System.out.println("Invoke get(3), value is: " + cache.get(3));
-        System.out.println("Invoke get(4), value is: " + cache.get(4));
-        cache.printDLList();
-    }
+    System.out.println();
+    System.out.println("Invoke get(1), value is: " + cache.get(1));
+    System.out.println("Invoke get(3), value is: " + cache.get(3));
+    System.out.println("Invoke get(4), value is: " + cache.get(4));
+    cache.printDLList();
+  }
 }
