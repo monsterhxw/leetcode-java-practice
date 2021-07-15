110846333852d09366541e602075266695a5e597
huangxuewei <huangxueweihxw@gmail.com>
2021-07-16 05:26:33 +0800

    add min heap solution.

--- leetcode-0215-kth-largest-element-in-an-array/src/algorithm/Test.java
+++ leetcode-0215-kth-largest-element-in-an-array/src/algorithm/Test.java
@@ -8,22 +8,30 @@ import java.util.Arrays;
  */
 public class Test {
   public static void main(String[] args) {
-    int[] nums1 = new int[] {3, 2, 1, 5, 6, 4};
-    int k1 = 2;
-    testMaxHeapSolution(nums1, k1);
-
-    int[] nums2 = new int[] {3, 2, 3, 1, 2, 4, 5, 5, 6};
-    int k2 = 4;
-    testMaxHeapSolution(nums2, k2);
+    int[] nums = new int[] {3, 2, 1, 5, 6, 4};
+    int k = 3;
+    testMaxHeapSolution(nums, k);
+    testMinHeapSolution(nums, k);
   }
 
   private static void testMaxHeapSolution(int[] nums, int k) {
     System.out.println("----------testMaxHeapSolution----------");
     print(nums, k);
     MaxHeapSolution solution = new MaxHeapSolution();
+    int[] copyOf = Arrays.copyOf(nums, nums.length);
     System.out.println(
-        "the k(" + k + ") th largest element is: " + solution.findKthLargest(nums, k));
-    System.out.println(Arrays.toString(nums));
+        "the k(" + k + ") th largest element is: " + solution.findKthLargest(copyOf, k));
+    System.out.println(Arrays.toString(copyOf));
+  }
+
+  private static void testMinHeapSolution(int[] nums, int k) {
+    System.out.println("-----------testMinHeapSolution---------");
+    print(nums, k);
+    MinHeapSolution solution = new MinHeapSolution();
+    int[] copyOf = Arrays.copyOf(nums, nums.length);
+    System.out.println(
+        "the k(" + k + ") th largest element is: " + solution.findKthLargest(copyOf, k));
+    System.out.println(Arrays.toString(copyOf));
   }
 
   private static void print(int[] nums, int k) {
