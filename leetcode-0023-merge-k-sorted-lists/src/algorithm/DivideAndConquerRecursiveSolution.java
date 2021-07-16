1d3cf82cee97d06465b7bdcc519571ee958db00a
huangxuewei <huangxueweihxw@gmail.com>
2021-07-17 03:58:34 +0800

    add divide and conquer recursive solution.

---
+++ leetcode-0023-merge-k-sorted-lists/src/algorithm/DivideAndConquerRecursiveSolution.java
@@ -0,0 +1,46 @@
+package algorithm;
+
+/**
+ * @author XueweiHuang
+ * @created 2021-07-16
+ */
+public class DivideAndConquerRecursiveSolution {
+  /** Time Complexity: O(); Space Complexity: O(); */
+  public ListNode mergeKLists(ListNode[] lists) {
+    return merge(lists, 0, lists.length - 1);
+  }
+
+  private ListNode merge(ListNode[] lists, int left, int right) {
+    if (left == right) {
+      return lists[left];
+    } else if (left > right) {
+      return null;
+    } else {
+      // left < right;
+      int mid = left + (right - left) / 2;
+      return mergeTwoLists(merge(lists, left, mid), merge(lists, mid + 1, right));
+    }
+  }
+
+  private ListNode mergeTwoLists(ListNode first, ListNode second) {
+    if (first == null || second == null) {
+      return first == null ? second : first;
+    }
+    ListNode head = new ListNode(-1);
+    ListNode tail = head;
+    ListNode firstPtr = first;
+    ListNode secondPtr = second;
+    while (firstPtr != null && secondPtr != null) {
+      if (firstPtr.val < secondPtr.val) {
+        tail.next = firstPtr;
+        firstPtr = firstPtr.next;
+      } else {
+        tail.next = secondPtr;
+        secondPtr = secondPtr.next;
+      }
+      tail = tail.next;
+    }
+    tail.next = firstPtr == null ? secondPtr : firstPtr;
+    return head.next;
+  }
+}
--- leetcode-0023-merge-k-sorted-lists/src/algorithm/Test.java
+++ leetcode-0023-merge-k-sorted-lists/src/algorithm/Test.java
@@ -1,33 +1,49 @@
 package algorithm;
 
-import java.util.Arrays;
-
 /**
  * @author XueweiHuang
  * @created 2021-07-16
  */
 public class Test {
   public static void main(String[] args) {
-    int[] one = new int[] {1, 4, 5};
-    int[] two = new int[] {1, 3, 4};
-    int[] three = new int[] {2, 6};
-    int[][] nums = new int[][] {one, two, three};
-    ListNode[] lists = new ListNode[nums.length];
-    System.out.println("lists is: ");
-    for (int i = 0; i < nums.length; i++) {
-      lists[i] = ListNode.createSinglyLinkedList(nums[i]);
-      ListNode.printLinkedList(lists[i]);
-    }
-    System.out.println();
+    testBruteForceSolution(creatListNodeArray());
 
-    testBruteForceSolution(lists);
+    testDivideAndConquerRecursiveSolution(creatListNodeArray());
   }
 
   private static void testBruteForceSolution(ListNode[] lists) {
     System.out.println("----------testBruteForceSolution-----------");
-    ListNode[] copyOf = Arrays.copyOf(lists, lists.length);
+    print(lists);
     BruteForceSolution solution = new BruteForceSolution();
     System.out.println("call mergeKLists: ");
-    ListNode.printLinkedList(solution.mergeKLists(copyOf));
+    ListNode.printLinkedList(solution.mergeKLists(lists));
+  }
+
+  private static void testDivideAndConquerRecursiveSolution(ListNode[] lists) {
+    System.out.println("----------testDivideAndConquerRecursiveSolution-----------");
+    print(lists);
+    DivideAndConquerRecursiveSolution solution = new DivideAndConquerRecursiveSolution();
+    System.out.println("call mergeKLists: ");
+    ListNode.printLinkedList(solution.mergeKLists(lists));
+  }
+
+  private static ListNode[] creatListNodeArray() {
+    int[] one = new int[] {1, 4, 5};
+    int[] two = new int[] {1, 3, 4};
+    int[] three = new int[] {2, 6, 8, 10, 78};
+    int[][] nums = new int[][] {one, two, three};
+    ListNode[] lists = new ListNode[nums.length];
+    for (int i = 0; i < nums.length; i++) {
+      lists[i] = ListNode.createSinglyLinkedList(nums[i]);
+    }
+    return lists;
+  }
+
+  private static void print(ListNode[] lists) {
+    System.out.println("lists is: ");
+    for (int i = 0; i < lists.length; i++) {
+      ListNode.printLinkedList(lists[i]);
+    }
+    System.out.println();
   }
 }
